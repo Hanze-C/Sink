1f882880d3a3128f6650170aea71d7f89ee43a4b
Hanze-C <hanze2010@outlook.com>
2025-01-19 20:46:07 +0800

    Update zh-CN Translation Ver0.9.3

--- components/home/Features.vue
+++ components/home/Features.vue
@@ -3,39 +3,39 @@ import { AreaChart, Hourglass, Link, Paintbrush, ServerOff, Sparkles } from 'luc
 
 const features = ref([
   {
-    title: 'URL Shortening',
+    title: '链接缩短',
     description:
-          'Compress your URLs to their minimal length.',
+          '将您的链接压缩到最小长度。',
     icon: Link,
   },
   {
-    title: 'Analytics',
+    title: '数据分析',
     description:
-          'Monitor link analytics and gather insightful statistics.',
+          '自动分析您的链接的访问情况。',
     icon: AreaChart,
   },
   {
-    title: 'Serverless',
+    title: '无服务器',
     description:
-          'Deploy without the need for traditional servers.',
+          '无需传统服务器即可部署。',
     icon: ServerOff,
   },
   {
-    title: 'Customizable Slug',
+    title: '自定义 Slug',
     description:
-          'Support for personalized slugs and case sensitivity.',
+          '支持自定义Slug并支持区分大小写。',
     icon: Paintbrush,
   },
   {
-    title: 'AI ',
+    title: 'AI Slug',
     description:
-          'Leverage AI to generate slugs.',
+          '使用Workers AI自动生成Slug。',
     icon: Sparkles,
   },
   {
-    title: 'Link Expiration',
+    title: '时间设定',
     description:
-          'Set expiration dates for your links.',
+          '为您的链接设置过期时间。',
     icon: Hourglass,
   },
 ])
